cab54bc9de5b43c6c889cce691dda039515d6cb1
llu5 <llu5@yahoo.com>
2016-07-10 22:17:50 -0400

    Third commit

--- server.R
+++ server.R
@@ -1,12 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 if (!require(RISmed)){ 
@@ -36,6 +27,22 @@ if (!require(RColorBrewer)){
 if (!require(plyr)){ 
   install.packages("plyr") 
 }
+
+if (!require(slidify)){ 
+  install_github('ramnathv/slidify', 'ramnathv', ref = 'dev', force = TRUE)
+}
+
+if (!require(rCharts)){ 
+  install_github('ramnathv/rCharts', 'ramnathv',  force = TRUE)
+}
+
+
+if (!require(slidifyLibraries)){ 
+  install_github('ramnathv/slidifyLibraries', 'ramnathv', ref = 'dev', force = TRUE) # optional
+}
+
+
+
 library(RISmed)
 library(ggplot2)
 library(slam)
@@ -43,6 +50,7 @@ library(devtools)
 library(RColorBrewer)
 library(wordcloud)
 library(plyr)
+
 # library(yplot)
 
 #tally each year beginning at 1970
@@ -52,7 +60,6 @@ library(plyr)
 #blocked from accessing NCBI.
 
 
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
@@ -60,10 +67,7 @@ shinyServer(function(input, output) {
   # res <- EUtilsSummary(input$id1+" + "+input$id3, type="esearch", db="pubmed", datetype='pdat', mindate=input$id4, maxdate=input$id5, retmax=50000)
   # qc<-QueryCount(res)
   # get the ratio of total number of two search results done
-  
-  
   # plot the trends of total number of pubmed results per year for each search in single plot with double Y axis  and lines of different color. Done.
-  
   # build and plot the linear model from total of pubmed results per year for each of the results
 
   output$oid1 = renderPrint({QueryCount(EUtilsSummary(paste(input$id1," AND ",input$id3), type="esearch", db="pubmed", datetype='pdat', mindate=input$id4, maxdate=input$id5, retmax=50000))})
@@ -75,6 +79,7 @@ shinyServer(function(input, output) {
        HTML(paste("<H2>Results</H2></br>Total number for publications on ", input$id3 ," research of ", input$id1, " is <b>", q1total, "</b>, and the total number of publications on ", input$id3 ," research of ",input$id2, " is <b>",q2total , "</b>.
                   </br> The ratio of total publications in Pubmed on ", input$id3, " research of ", input$id1," to ", input$id2 , " between ", input$id4, " and ", input$id5, " is: <b>", round(ratioq1toq2, digits = 3) ,"</b>",sep = ' '))
        })
+  
   output$plot1 <- renderPlot({
     validate(
       need(input$id1 != "", "Please enter a valid search term.")
@@ -139,21 +144,19 @@ shinyServer(function(input, output) {
       
       year <- number <- TERM <- NULL
       
-      ## if (is(x, "list")) {
-      
-      ##     df <- ldply(x)
-      
-      ##     colnames(df)[1] <- "TERM"
+
       
       if (ncol(x) == 3 && "TERM" %in% colnames(x)) {
         
-        p <- ggplot(x, aes(factor(year), number, group=TERM, color=TERM))+
+        p <- ggplot(x, aes(factor(year), number, group=TERM, color=TERM))
+        p <-p  + labs(x=paste("Year & Query date: ", Sys.time(), sep=""), y="Number of articles" )
           
-          geom_point(size=3) + geom_line()+geom_smooth(method="lm")
-        
+        p <- p + geom_point(size=3) + geom_line()+geom_smooth(method="lm") 
       } else {
         
-        p <- ggplot(x, aes(year, number))+geom_point(size=3) + geom_line()+geom_smooth(method="lm")
+        p <- ggplot(x, aes(year, number))+geom_point(size=3)
+        p <-p +labs(x=paste("Year & Query date: ", Sys.time(), sep=""), y="Number of articles" )
+        p <-p + geom_line()+geom_smooth(method="lm")
         
       }
       
@@ -170,13 +173,67 @@ shinyServer(function(input, output) {
     
   })
 
+
+  
   # Generate a summary of the data
 
   output$summary <- renderPrint({
+    
+    getPubmedTrend.internal <- function(searchTerm, year) {
+      withProgress(message = 'Making plot', value = 0, {
+        # Number of times we'll go through the loop
+        n <- as.numeric(input$id5)-as.numeric(input$id4)
+        
+        num <- array()
+        
+        x <- 1
+        
+        for (i in year){
+          
+          Sys.sleep(1)
+          
+          cat("querying year ", i, "\n")
+          
+          r <- EUtilsSummary(searchTerm, type='esearch', db='pubmed', mindate=i, maxdate=i)
+          
+          num[x] <- QueryCount(r)
+          # Increment the progress bar, and update the detail text.
+          incProgress(1/n, detail = paste("Doing part", i))
+          
+          
+          x <- x + 1
+          
+        }
+        
+      })
+      res <- data.frame(year=year, number=num)
+      
+      return(res)
+      
+    }
+    
+    getPubmedTrend <- function(searchTerm, year) {
+      
+      if (length(searchTerm) == 1)
+        
+        return(getPubmedTrend.internal(searchTerm, year))
+      
+      res <- lapply(searchTerm, getPubmedTrend.internal, year=year)
+      
+      names(res) <- searchTerm
+      
+      res.df <- ldply(res)
+      
+      colnames(res.df)[1] <- "TERM"
+      
+      return(res.df)
+      
+    }
+    
     ifall<-if(input$id3 !="") " AND " else "";
     term = c(paste(input$id1, ifall,input$id3), paste(input$id2, ifall,input$id3))
-    pm=getPubmedTrend(term, year=as.numeric(input$id4):input$id5)
-    summary(pm)
+    getPubmedTrend(term, year=as.numeric(input$id4):input$id5)
+
   })
   
   # Generate a supproting documentation
@@ -199,6 +256,6 @@ shinyServer(function(input, output) {
     There are three tabs in the main panel. The plot tab displays a line chart for two search result in given time period.
     Before plot is completed, a progress bar is displayed to notify user that the data is being processed.
     The Summary tab display year of search results and a summary of all data used for the plot.
-    The third tab is Total numbers and Ratio. It describe the results of total numbers and calculatied ratio of total numbers of the two searches.")
+    The third tab is Total numbers and Ratio. It describe the results of total numbers and calculatied ratio of total numbers of the two searches.Please click the tab and Submit button and wait for a few seconds to see the result. ")
   })
 })
--- ui.R
+++ ui.R
@@ -1,16 +1,5 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
-
-
-
 # Define UI for application that draws a line chart.
 shinyUI(fluidPage(
   
@@ -64,17 +53,13 @@ shinyUI(fluidPage(
     # Show a plot of the generated distribution
     mainPanel(
       h3('Search of Pubmed with two terms and year of publication:'),
-     
-      h4('Ratio of total numbers of results'),
-     
- 
- 
 
       tabsetPanel(
         tabPanel("Plot", plotOutput("plot1")), 
-        tabPanel("Summary", verbatimTextOutput("summary")), 
+        tabPanel("Summary of Data", verbatimTextOutput("summary")), 
         tabPanel("Result and Documentation",  htmlOutput("oid3"),
-        tabPanel("Suporting Documentation", htmlOutput("documentation") )       
+        tabPanel("Documentation",  htmlOutput("documentation")
+         )       
                  )
       )
       
